e26271adfc030c0112bede71426db90801a66e3f
sahvsergio <sergioandresherrera@gmail.com>
2024-12-20 17:04:39 -0500

    create movies.csv file

--- movie_requests.py
+++ movie_requests.py
@@ -14,6 +14,8 @@ def request_data():
     # Create list to store movie ids
     flick_ids=[]
     # reading the original file 
+
+    
     with open('oscar_winners.csv','r') as oscar_file:
         try:
             
@@ -26,42 +28,39 @@ def request_data():
         except FileNotFoundError:
             print('The Oscar files have not been found')
     # get  movie info from ip
-    for movie_id in flick_ids:
-        try:
-            payload = {'apikey': api_key, 'i': movie_id}
-            res= requests.get('http://www.omdbapi.com/',params=payload)
-               
-        except Exception as e:
-            print(e)
-        res_json=res.json()
-        #print(res_json)
-        
-        movie_title:str =res_json['Title']
-        movie_runtime:int = int(res_json['Runtime'].strip('').strip('min'))
-        movie_genre:str = res_json['Genre']
-       
-        movie_awards = res_json['Awards'].split(' ')
-        movie_oscars=int(movie_awards[1])
-        movie_wins=int(movie_awards[3])
-        movie_nominations=int(movie_awards[6])
-        movie_box_office=int(res_json['BoxOffice'].strip('$').replace(',', ''))
-        print(type(movie_box_office))
-        
+
+    fieldnames =[
+        'Title',
+        'Runtime',
+        'Genre',
+        'Oscars',
+        'Award Wins',
+        'Award Nominations',
+        'Box Office'
+    ]
     with open('movies.csv', 'w') as movie_file:
-        fieldnames = [
-            'Movie Title',
-            'Runtime',
-            'Genre',
-            'Award Wins' ,
-            'Award Nominations',
-            'Box Office' 
-            ]
-        movie_writer=csv.DictWriter(movie_file, fieldnames=fieldnames)
+
+        movie_writer = csv.DictWriter(movie_file, fieldnames=fieldnames)
         movie_writer.writeheader()
-    
+        for movie_id in flick_ids:
+            try:
+                payload = {'apikey': api_key, 'i': movie_id}
+                res= requests.get('http://www.omdbapi.com/',params=payload)
+                res_json = res.json()
+                movie_data = {
+                    'Title': res_json['Title'],
+                    'Runtime': int(res_json['Runtime'].strip('').strip('min')),
+                    'Genre': res_json['Genre'],
+                    'Oscars': int(res_json['Awards'].split(' ')[1]),
+                    'Award Wins': int(res_json['Awards'].split(' ')[3]),
+                    'Award Nominations': int(res_json['Awards'].split(' ')[6]),
+                    'Box Office': int(res_json['BoxOffice'].strip('$').replace(',', ''))
+                }
+                movie_writer.writerow(movie_data)
+               
+            except Exception as e:
+                print(f'Error processing movie{movie_id}:{e}')
 
 
 
-
- 
 request_data()
